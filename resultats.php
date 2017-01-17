e8fc9b70bc4a7feee7f0ec72ff4d7b6fc8db0eb6
Noemie <noemie.clay@hotmail.fr>
2017-01-17 11:26:33 +0100

    Suite template

---
+++ resultats.php
@@ -0,0 +1,35 @@
+<?php
+include('vue/haut.inc.php');
+?>
+
+<div class="row">
+  Résultats
+</div>
+<div class="row resultats">
+  <div class="head">
+    <div class="col-md-2">
+      Départ
+    </div>
+    <div class="col-md-2">
+      Heure de départ
+    </div>
+    <div class="col-md-2">
+      Arrivée
+    </div>
+    <div class="col-md-2">
+      Heure d'arrivée
+    </div>
+    <div class="col-md-2">
+      Temps
+    </div>
+    <div class="col-md-2">
+      Prix
+    </div>
+  </div>
+  <div class="datavols">
+
+  </div>
+</div>
+<a class="new-search" href="index.php">
+  Nouvelle recherche
+</a>
---
+++ vue/haut.inc.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <title>
+      AirCheck
+    </title>
+    <!-- Bootstrap Core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <!--Haut du site-->
+      <div class="row haut">
+        <div class="col-md-4">
+          <h1>
+            AirCheck
+          </h1>
+        </div>
+        <div class="col-md-2 col-md-offset-6">
+          <a href="connexion.php">Connexion</a>
+        </div>
+      </div>
+      <!-- -->
+      <div class="row slogan">
+        <p>
+          Consultez les vols disponibles dans vos aéroports!
+        </p>
+      </div>
